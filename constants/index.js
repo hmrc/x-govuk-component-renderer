36e0d7c49a416da28a4c0ca244aab946a13d040c
Silas <1029540+silasl@users.noreply.github.com>
2019-10-23 10:09:47 +0100

    PLATUI-256: Constants

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  govukFrontendRoot: path.resolve('dependencies/govuk-frontend'),
   designSystemRoot: path.resolve('dependencies/alphagov/govuk-design-system'),
   readMe: path.resolve('README.md'),
   substitutionMap: {
