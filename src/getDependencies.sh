6e4146b9913ac95fa404cbd48e76967ecacbeb26
omair-ali <omair.ali1@digital.hmrc.gov.uk>
2020-02-14 12:34:46 +0000

    PLATUI-286: removed invalid option in get dependencies retries command

--- src/getDependencies.sh
+++ src/getDependencies.sh
@@ -7,10 +7,11 @@ mkdir -p "$4/dependencies"
 
 cd "$4/dependencies"
 
-wget -O dep.tar.gz "$2" --retry-on-http-error=404,429,503,504 --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 -O dep.tar.gz "$2"
 
 mkdir -p "$1" && tar xf dep.tar.gz -C "$1" --strip-components 1
 rm -f dep.tar.gz
 
 cd "$1"
 echo "$3" > version.txt
+echo "$0" "$1" "$2" "$3" "$4" > command.txt
